c7c1b7bb12a3d9861bf91c461291da843079f809
SirZeni <115522316+SirZeni@users.noreply.github.com>
2023-10-27 19:29:14 -0500

    Update sirzenigamingactions.js

--- sirzenigamingactions.js
+++ sirzenigamingactions.js
@@ -32,3 +32,88 @@ function scrollFunction() {
   }
 }
 }
+
+function showQR(){
+var x = document.getElementById("QRcodeimg1");
+var y = document.getElementById("QRcodeimg1download");
+ if (x.style.display === "none"){
+  x.style.display = "block";
+ } else {
+  x.style.display = "none";
+ }
+
+ if (y.style.display === "none"){
+  y.style.display = "block";
+ } else {
+  y.style.display = "none";
+ }
+
+}
+
+function proceedtopatchnotes(){
+window.alert("Proceed to Patch Notes?");
+document.location = "sirzenigamingpnotes.html";
+}
+
+function toggleshowcinemamenu(){
+var a = document.getElementById("cinemadropdownmenu");
+
+if(a.style.display === "none"){
+  a.style.display = "block";
+} else{
+  a.style.display = "none";
+}
+}
+
+function gotovideo1(){
+  let mybutton = document.getElementById("cinemabutton1");
+  var a = document.getElementById("cinemayoutubevideo1");
+  var b = document.getElementById("cinemayoutubevideo2");
+  var c = document.getElementById("cinemayoutubevideo3");
+  var d = document.getElementById("cinemayoutubevideo4");
+  if(a.style.display = "none"){
+    a.style.display = "block";
+    b.style.display = "none";
+    c.style.display = "none";
+    d.style.display = "none";
+}
+}
+
+function gotovideo2(){
+  var a = document.getElementById("cinemayoutubevideo1");
+  var b = document.getElementById("cinemayoutubevideo2");
+  var c = document.getElementById("cinemayoutubevideo3");
+  var d = document.getElementById("cinemayoutubevideo4");
+  if(b.style.display = "none"){
+    b.style.display = "block";
+    a.style.display = "none";
+    c.style.display = "none";
+    d.style.display = "none";
+}
+}
+
+function gotovideo3(){
+  var a = document.getElementById("cinemayoutubevideo1");
+  var b = document.getElementById("cinemayoutubevideo2");
+  var c = document.getElementById("cinemayoutubevideo3");
+  var d = document.getElementById("cinemayoutubevideo4");
+  if(c.style.display = "none"){
+    c.style.display = "block";
+    a.style.display = "none";
+    b.style.display = "none";
+    d.style.display = "none";
+}
+}
+
+function gotovideo4(){
+  var a = document.getElementById("cinemayoutubevideo1");
+  var b = document.getElementById("cinemayoutubevideo2");
+  var c = document.getElementById("cinemayoutubevideo3");
+  var d = document.getElementById("cinemayoutubevideo4");
+  if(d.style.display = "none"){
+    d.style.display = "block";
+    a.style.display = "none";
+    b.style.display = "none";
+    c.style.display = "none";
+}
+}
